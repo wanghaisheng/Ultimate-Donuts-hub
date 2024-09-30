567c9ba75e171e1af2b3d6b6269c9061390b2956
ShaherAshraf <shaherashraf77@gmail.com>
2024-10-01 00:44:29 +0300

    Fix a style issue with the mobile nav component & animate it using angular animations

--- src/app/layouts/header/components/mobile-nav/mobile-nav.component.ts
+++ src/app/layouts/header/components/mobile-nav/mobile-nav.component.ts
@@ -4,6 +4,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { NavListComponent } from '../nav-list/nav-list.component';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-mobile-nav',
@@ -18,12 +25,30 @@ import { NavListComponent } from '../nav-list/nav-list.component';
   ],
   templateUrl: './mobile-nav.component.html',
   styleUrl: './mobile-nav.component.scss',
+  animations: [
+    trigger('drawerState', [
+      state(
+        'open',
+        style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'translateX(100%)',
+          opacity: 0,
+        })
+      ),
+      transition('open <=> closed', animate('300ms ease-in-out')),
+    ]),
+  ],
 })
 export class MobileNavComponent {
   isVisible = false;
 
   toggleMenu() {
-    debugger;
     this.isVisible = !this.isVisible;
   }
 }
