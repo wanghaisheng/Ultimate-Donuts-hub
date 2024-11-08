3b1591d56eca9e97a495392d2dade0e3ac78fc90
ShaherAshraf <shaherashraf77@gmail.com>
2024-11-08 22:50:27 +0200

    Stop tracking environment files.

--- frontend/src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  supabaseUrl: 'https://btsgadhhjuwuupnolpbz.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0c2dhZGhoanV3dXVwbm9scGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5Mjk4MjYsImV4cCI6MjA0NDUwNTgyNn0.JKbq8ue2ufhIt2JdLZ2AuF6VM0PecjbAWn2YaId5RBI',
-  STRIPE_PUBLISHABLE_KEY:
-    'pk_test_51OuNP1HHOG9PFvc89Di0uxA7jPpQHqPP9MwbUTaDzYHMVG4i57JKA1CscGK5KHD30XMKuuNydchN6c6hHyBo08f300pAh9UgTu',
-  STRIPE_SECRET_KEY:
-    'sk_test_51OuNP1HHOG9PFvc8Kpmelc5HyvX4J9YxkTEGaFZCqqiLjaDHCz3bF2Kmu6dZrAsJTSEve8BhX4iVDCJR7hC3IsFQ00GpcBvvqQ',
-};
